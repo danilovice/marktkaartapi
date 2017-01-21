5f1e649f06fa1bb373cc986bbf4bc22ea4248a44
Daniel Koeman <Daniel Koeman>
2017-01-21 11:39:53 +0100

    api methodes voltooid met update en remove

--- Controllers/MarktController.cs
+++ Controllers/MarktController.cs
@@ -15,6 +15,7 @@ namespace Marktkaart.Controllers
             this._repository = repository;
         }
 
+        // Get all items
         [HttpGet]
         public IEnumerable<Markt> GetAll()
         {
@@ -22,6 +23,7 @@ namespace Marktkaart.Controllers
             return result;
         }
 
+        // Get item by Id (guid)
         [HttpGet("{id}", Name = "GetMarkt")]
         public IActionResult GetById(string id)
         {
@@ -33,6 +35,7 @@ namespace Marktkaart.Controllers
             return new ObjectResult(item);
         }
 
+        // Create new item
         [HttpPost]
         public IActionResult Create([FromBody] Markt item)
         {
@@ -42,6 +45,37 @@ namespace Marktkaart.Controllers
             }
             _repository.Add(item);
             return CreatedAtRoute("GetMarkt", new { id = item.Guid }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] Markt item)
+        {
+            if (item == null || item.Guid != id)
+            {
+                return BadRequest();
+            }
+
+            var markt = _repository.Find(id);
+            if (markt == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var markt = _repository.Find(id);
+            if (markt == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Remove(id);
+            return new NoContentResult();
         }
     }
 }
